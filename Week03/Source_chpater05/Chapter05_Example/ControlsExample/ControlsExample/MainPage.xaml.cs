5fcf72e25cc75f57e8c22bbed69ad466802a7bde
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-10-31 22:40:10 +0700

    Add SourceCode_chapter05

---
+++ Week03/Source_chpater05/Chapter05_Example/ControlsExample/ControlsExample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ControlsExample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async void navigateToPage(object sender, EventArgs args)
+        {
+            String pageName = (String)((TextCell)sender).CommandParameter;
+            Type pageType = Type.GetType("ControlsExample.Views." + pageName);
+            Page page = (Page)Activator.CreateInstance(pageType);
+            await this.Navigation.PushAsync(page);
+        }
+    }
+
+}
---
+++ Week03/Source_chpater05/Chapter05_Example/ControlsExample/ControlsExample/Views/Controls.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlsExample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Controls : ContentPage
+    {
+        public Controls()
+        {
+            InitializeComponent();
+        }
+
+        protected void PickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            PageValue.Text = (string)ThePicker.ItemsSource[ThePicker.SelectedIndex];
+        }
+
+        protected void DatePickerDateSelected(object sender, DateChangedEventArgs e)
+        {
+            EventValue.Text = e.NewDate.ToString();
+            PageValue.Text = TheDatePicker.Date.ToString();
+        }
+
+        //protected void TimePickerPropertyChanged(object sender, PropertyChangingEventArgs e)
+        //{
+        //    if (e.PropertyName == TimePicker.TimeProperty.PropertyName)
+        //    {
+        //        PageValue.Text = TheTimePicker.Time.ToString();
+        //    }
+        //}
+
+        protected void StepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            EventValue.Text = String.Format("Stepper value is {0:F1}", e.NewValue);
+            PageValue.Text = TheStepper.Value.ToString();
+        }
+
+        protected void SliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            EventValue.Text = String.Format("Slider value is {0:F1}", e.NewValue);
+            PageValue.Text = TheSlider.Value.ToString();
+        }
+
+        protected void SwitchToggled(object sender, ToggledEventArgs e)
+        {
+            EventValue.Text = String.Format("Switch is now {0}", e.Value ? "On" : "Off");
+            PageValue.Text = TheSwitch.IsToggled.ToString();
+        }
+    }
+}
